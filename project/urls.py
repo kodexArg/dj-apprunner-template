768c727ea51b4952c633778511eb6132ec37f09b
kodexArg <gcavedal@gmail.com>
2025-06-06 20:40:55 -0300

    Arreglo en el Home

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from core.views import home
 
 urlpatterns = [
-    # Temporary root redirect - Remove this when main app is defined
-    path('', RedirectView.as_view(url='/core/home/', permanent=False)),
-    
+    path('', home, name='root_home'),
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
