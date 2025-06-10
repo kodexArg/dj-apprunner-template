0941b245db2ee98a35b8c378ec3525d9275a2138
kodexArg <gcavedal@gmail.com>
2025-06-10 11:05:33 -0300

    Mejor core/home.html

--- core/views.py
+++ core/views.py
@@ -1,20 +1,36 @@
 from django.http import HttpResponse
 import os
+import sys
+import django
 from django.db import connection
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.conf import settings
 
 def home(request):
     """
-    Vista principal de la aplicación.
+    Vista principal de la aplicación con información detallada del sistema.
     
     Args:
         request: Objeto HttpRequest de Django
         
     Returns:
-        HttpResponse: Renderiza la plantilla home.html
+        HttpResponse: Renderiza la plantilla home.html con información del sistema
     """
-    return render(request, 'core/home.html')
+    context = {
+        'django_version': django.get_version(),
+        'python_version': sys.version.split()[0],
+        'csrf_enabled': settings.CSRF_COOKIE_SECURE,
+        'https_enabled': settings.SECURE_PROXY_SSL_HEADER is not None,
+        'debug_mode': settings.DEBUG,
+        'storage_backend': 'S3' if not settings.IS_LOCAL else 'Local',
+        'cdn_enabled': bool(settings.AWS_S3_CUSTOM_DOMAIN),
+        's3_bucket_name': settings.AWS_STORAGE_BUCKET_NAME,
+        'environment': 'Desarrollo' if settings.IS_LOCAL else 'Producción',
+        'aws_region': settings.AWS_S3_REGION_NAME,
+        'server_info': 'Gunicorn' if not settings.IS_LOCAL else 'Django Development Server'
+    }
+    return render(request, 'core/home.html', context)
 
 def hello_world(request):
     """
